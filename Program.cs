e7043654e411926b7d603b9d1c15bb679559a3ca
alejanf2885 <alumno.648000@ies-azarquiel.es>
2026-06-03 11:09:14 +0200

    Use AWS Secrets Manager for DB connection details
    Updated the application to dynamically retrieve database
    connection details (host, username, password) from AWS
    Secrets Manager. Hardcoded `dbname` to "zapatillas".
    Removed `builder.Services.AddControllersWithViews()`
    to reflect changes in controller/view handling.

--- Program.cs
+++ Program.cs
@@ -14,25 +14,26 @@ string region = "us-east-2";
 AmazonSecretsManagerClient client = new AmazonSecretsManagerClient(
     RegionEndpoint.GetBySystemName(region)
 );
+
 GetSecretValueRequest request = new GetSecretValueRequest
 {
     SecretId = secretName,
     VersionStage = "AWSCURRENT",
 };
+
 GetSecretValueResponse response = await client.GetSecretValueAsync(request);
 JsonDocument secret = JsonDocument.Parse(response.SecretString);
 
 string host = secret.RootElement.GetProperty("host").GetString();
 string username = secret.RootElement.GetProperty("username").GetString();
 string password = secret.RootElement.GetProperty("password").GetString();
-string dbname = secret.RootElement.GetProperty("dbname").GetString();
+string dbname = "zapatillas";
 
 string connectionString =
     $"Server={host};Port=3306;Database={dbname};User={username};Password={password};";
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
 builder.Services.AddScoped<RepositoryZapatillas>();
-
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
