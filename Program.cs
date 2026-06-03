b35df7c036398647564b31335193b7dc618957e4
alejanf2885 <alumno.648000@ies-azarquiel.es>
2026-06-03 11:05:46 +0200

    Integrate AWS Secrets Manager for DB credentials
    Replaced hardcoded database connection string with dynamic
    retrieval from AWS Secrets Manager. Added `System.Text.Json`
    and AWS SDK namespaces to support secret fetching and JSON
    parsing. Removed unused `MvcZapatillasExamenAnf.Models`
    namespace. Updated `AppDbContext` to use the dynamically
    constructed connection string. Cleaned up comments in
    Program.cs.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,42 @@
+using System.Text.Json;
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
 using Microsoft.EntityFrameworkCore;
 using MvcZapatillasExamenAnf.Data;
-using MvcZapatillasExamenAnf.Models;
 using MvcZapatillasExamenAnf.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
+string secretName = "zapatillas/rds-connection";
+string region = "us-east-2";
 
+AmazonSecretsManagerClient client = new AmazonSecretsManagerClient(
+    RegionEndpoint.GetBySystemName(region)
+);
+GetSecretValueRequest request = new GetSecretValueRequest
+{
+    SecretId = secretName,
+    VersionStage = "AWSCURRENT",
+};
+GetSecretValueResponse response = await client.GetSecretValueAsync(request);
+JsonDocument secret = JsonDocument.Parse(response.SecretString);
+
+string host = secret.RootElement.GetProperty("host").GetString();
+string username = secret.RootElement.GetProperty("username").GetString();
+string password = secret.RootElement.GetProperty("password").GetString();
+string dbname = secret.RootElement.GetProperty("dbname").GetString();
+
+string connectionString =
+    $"Server={host};Port=3306;Database={dbname};User={username};Password={password};";
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
 builder.Services.AddScoped<RepositoryZapatillas>();
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
